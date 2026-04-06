62924334f7e769e2808ada5c394972766422e5dc
Michael Lee <jkmcl@users.noreply.github.com>
2026-04-06 20:22:38 +0800

    Make script more robust

--- src/main/scripts/downloader.sh
+++ src/main/scripts/downloader.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DOWNLOADER_HOME="${BASH_SOURCE%/*}"
+DOWNLOADER_HOME="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 if [[ -z "$JAVA_HOME" ]]; then
 	echo 'The required JAVA_HOME environment variable is not defined' >&2
-	exit 1	
+	exit 1
 fi
 
 "$JAVA_HOME/bin/java" -jar "$DOWNLOADER_HOME/@project.build.finalName@.jar" "$@"
